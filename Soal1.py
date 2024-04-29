fdce0560d08dbf02770ee18c8813afeb1cdf453b
Bayuish <bayul769@gmail.com>
2024-04-29 18:50:14 +0700

    Menambahkan Jawaban Soal1

---
+++ Soal1.py
@@ -0,0 +1,6 @@
+print (" === Soal Bagian 2 Nomor 1 ===")
+Angka = int(input("Masukan Angka : "))
+if Angka % 2 == 0:
+    print (Angka,"Adalah Bilangan Genap") 
+else:
+    print (Angka," adalah bilangan ganjil")
